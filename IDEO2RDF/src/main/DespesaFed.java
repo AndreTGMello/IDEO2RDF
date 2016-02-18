d2f7e7e17d4556f0a7680f076e9446d1e9caad1b
André Mello <andre.tgmello@gmail.com>
2016-02-18 12:28:32 -0200

    Classe para objeto despesa federal.

---
+++ IDEO2RDF/src/main/DespesaFed.java
@@ -0,0 +1,239 @@
+package main;
+
+public class DespesaFed {
+	private int id;
+	
+	private String programa;
+	private String acao;
+	private String categoriaDespesa;
+	private String funcao;
+	private String mes;
+	private int ano;
+	
+	private String tipoDespesa;
+	private String portador;
+	private String transacao;
+	private String uf;
+	private String orgaoSuperior;
+	private String unidadeGestora;
+	private String orgao;
+	private String nroEmpenho;
+	private String nroConvenio;
+	private String convenete;
+	private String municipio;
+	private String credor;
+	private String modalidadeDespesa;
+	private String dataPagamento;
+	private String linguagemCidada;
+	private String elementoDespesa;
+	private String itemDespesa;
+	private String executor;
+	private String grupoDespesa;
+	private String favorecidoBolsaFamilia;
+	private String fonteRecurso;
+	private String situacaoParcel;
+	private String subfuncao;
+	private String despesa;
+	private String subtipoDespesa;
+	private String repasse;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPrograma() {
+		return programa;
+	}
+	public void setPrograma(String programa) {
+		this.programa = programa;
+	}
+	public String getAcao() {
+		return acao;
+	}
+	public void setAcao(String acao) {
+		this.acao = acao;
+	}
+	public String getCategoriaDespesa() {
+		return categoriaDespesa;
+	}
+	public void setCategoriaDespesa(String categoriaDespesa) {
+		this.categoriaDespesa = categoriaDespesa;
+	}
+	public String getFuncao() {
+		return funcao;
+	}
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+	public String getMes() {
+		return mes;
+	}
+	public void setMes(String mes) {
+		this.mes = mes;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public String getTipoDespesa() {
+		return tipoDespesa;
+	}
+	public void setTipoDespesa(String tipoDespesa) {
+		this.tipoDespesa = tipoDespesa;
+	}
+	public String getPortador() {
+		return portador;
+	}
+	public void setPortador(String portador) {
+		this.portador = portador;
+	}
+	public String getTransacao() {
+		return transacao;
+	}
+	public void setTransacao(String transacao) {
+		this.transacao = transacao;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	public String getOrgaoSuperior() {
+		return orgaoSuperior;
+	}
+	public void setOrgaoSuperior(String orgaoSuperior) {
+		this.orgaoSuperior = orgaoSuperior;
+	}
+	public String getUnidadeGestora() {
+		return unidadeGestora;
+	}
+	public void setUnidadeGestora(String unidadeGestora) {
+		this.unidadeGestora = unidadeGestora;
+	}
+	public String getOrgao() {
+		return orgao;
+	}
+	public void setOrgao(String orgao) {
+		this.orgao = orgao;
+	}
+	public String getNroEmpenho() {
+		return nroEmpenho;
+	}
+	public void setNroEmpenho(String nroEmpenho) {
+		this.nroEmpenho = nroEmpenho;
+	}
+	public String getNroConvenio() {
+		return nroConvenio;
+	}
+	public void setNroConvenio(String nroConvenio) {
+		this.nroConvenio = nroConvenio;
+	}
+	public String getConvenete() {
+		return convenete;
+	}
+	public void setConvenete(String convenete) {
+		this.convenete = convenete;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+	public String getCredor() {
+		return credor;
+	}
+	public void setCredor(String credor) {
+		this.credor = credor;
+	}
+	public String getModalidadeDespesa() {
+		return modalidadeDespesa;
+	}
+	public void setModalidadeDespesa(String modalidadeDespesa) {
+		this.modalidadeDespesa = modalidadeDespesa;
+	}
+	public String getDataPagamento() {
+		return dataPagamento;
+	}
+	public void setDataPagamento(String dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	public String getLinguagemCidada() {
+		return linguagemCidada;
+	}
+	public void setLinguagemCidada(String linguagemCidada) {
+		this.linguagemCidada = linguagemCidada;
+	}
+	public String getElementoDespesa() {
+		return elementoDespesa;
+	}
+	public void setElementoDespesa(String elementoDespesa) {
+		this.elementoDespesa = elementoDespesa;
+	}
+	public String getItemDespesa() {
+		return itemDespesa;
+	}
+	public void setItemDespesa(String itemDespesa) {
+		this.itemDespesa = itemDespesa;
+	}
+	public String getExecutor() {
+		return executor;
+	}
+	public void setExecutor(String executor) {
+		this.executor = executor;
+	}
+	public String getGrupoDespesa() {
+		return grupoDespesa;
+	}
+	public void setGrupoDespesa(String grupoDespesa) {
+		this.grupoDespesa = grupoDespesa;
+	}
+	public String getFavorecidoBolsaFamilia() {
+		return favorecidoBolsaFamilia;
+	}
+	public void setFavorecidoBolsaFamilia(String favorecidoBolsaFamilia) {
+		this.favorecidoBolsaFamilia = favorecidoBolsaFamilia;
+	}
+	public String getFonteRecurso() {
+		return fonteRecurso;
+	}
+	public void setFonteRecurso(String fonteRecurso) {
+		this.fonteRecurso = fonteRecurso;
+	}
+	public String getSituacaoParcel() {
+		return situacaoParcel;
+	}
+	public void setSituacaoParcel(String situacaoParcel) {
+		this.situacaoParcel = situacaoParcel;
+	}
+	public String getSubfuncao() {
+		return subfuncao;
+	}
+	public void setSubfuncao(String subfuncao) {
+		this.subfuncao = subfuncao;
+	}
+	public String getDespesa() {
+		return despesa;
+	}
+	public void setDespesa(String despesa) {
+		this.despesa = despesa;
+	}
+	public String getSubtipoDespesa() {
+		return subtipoDespesa;
+	}
+	public void setSubtipoDespesa(String subtipoDespesa) {
+		this.subtipoDespesa = subtipoDespesa;
+	}
+	public String getRepasse() {
+		return repasse;
+	}
+	public void setRepasse(String repasse) {
+		this.repasse = repasse;
+	}
+	
+}

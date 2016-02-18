dfd1c7eae4b365d9d1ee0fe6209c877d7bcfbb72
André Mello <andre.tgmello@gmail.com>
2016-02-18 08:52:23 -0200

    Classe para objeto Receita Federal.

---
+++ IDEO2RDF/src/main/ReceitaFed.java
@@ -0,0 +1,84 @@
+package main;
+
+public class ReceitaFed {
+	private String subalinea;
+	private String alinea;
+	private String rubrica;
+	private String especie;
+	private String origem;
+	private String categoriaEconomica;
+	private int ano;
+	private String mes;
+	
+	private String data;
+	private String orgaoSubordinado;
+	private String orgaoSuperior;
+	
+	public String getSubalinea() {
+		return subalinea;
+	}
+	public void setSubalinea(String subalinea) {
+		this.subalinea = subalinea;
+	}
+	public String getAlinea() {
+		return alinea;
+	}
+	public void setAlinea(String alinea) {
+		this.alinea = alinea;
+	}
+	public String getRubrica() {
+		return rubrica;
+	}
+	public void setRubrica(String rubrica) {
+		this.rubrica = rubrica;
+	}
+	public String getEspecie() {
+		return especie;
+	}
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	public String getOrigem() {
+		return origem;
+	}
+	public void setOrigem(String origem) {
+		this.origem = origem;
+	}
+	public String getCategoriaEconomica() {
+		return categoriaEconomica;
+	}
+	public void setCategoriaEconomica(String categoriaEconomica) {
+		this.categoriaEconomica = categoriaEconomica;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public String getMes() {
+		return mes;
+	}
+	public void setMes(String mes) {
+		this.mes = mes;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getOrgaoSubordinado() {
+		return orgaoSubordinado;
+	}
+	public void setOrgaoSubordinado(String orgaoSubordinado) {
+		this.orgaoSubordinado = orgaoSubordinado;
+	}
+	public String getOrgaoSuperior() {
+		return orgaoSuperior;
+	}
+	public void setOrgaoSuperior(String orgaoSuperior) {
+		this.orgaoSuperior = orgaoSuperior;
+	}
+	
+}

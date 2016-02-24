a174bd56ebf8f342347047f90757c29adbce0142
André Mello <andre.tgmello@gmail.com>
2016-02-24 14:27:30 -0300

    Erro da consulta SQL resolvido.

--- IDEO2RDF/src/main/Conversor.java
+++ IDEO2RDF/src/main/Conversor.java
@@ -52,20 +52,20 @@ public class Conversor {
 	public static ArrayList<ReceitaFed> populaReceitasFederais(Connection con) throws SQLException{
 		// Funcao que realiza a query no banco de dados para obter todas as informacoes referentes a Receitas Federais
 		PreparedStatement stmt = con.prepareStatement(
-				"SELECT * FROM fato_receita_federal "
-						+ "JOIN d_rf_origem "
-						+ "JOIN d_rf_especie "
-						+ "JOIN d_rf_rubrica "
-						+ "JOIN d_rf_alinea "
-						+ "JOIN d_rf_subalinea "
-						+ "JOIN d_rf_orgao_superior "
-						+ "JOIN d_rf_orgao_subordinadof "
-						+ "JOIN d_rf_unidade_gestora "
-						+ "JOIN d_rf_data "
-						+ "JOIN d_rf_mes "
-						+ "JOIN d_rf_ano "
-						+ "JOIN d_rf_categoria_economica "
-						+ "LIMIT 20");
+				"SELECT * FROM fato_receita_federal"
+						+ " INNER JOIN d_rf_origem ON fato_receita_federal.id_origem_d_rf = d_rf_origem.id_origem_d_rf "
+						+ " INNER JOIN d_rf_especie ON fato_receita_federal.id_especie_d_rf = d_rf_especie.id_especie_d_rf "
+						+ " INNER JOIN d_rf_rubrica ON fato_receita_federal.id_rubrica_d_rf = d_rf_rubrica.id_rubrica_d_rf "
+						+ " INNER JOIN d_rf_alinea ON fato_receita_federal.id_alinea_d_rf = d_rf_alinea.id_alinea_d_rf "
+						+ " INNER JOIN d_rf_subalinea ON fato_receita_federal.id_subalinea_d_rf = d_rf_subalinea.id_subalinea_d_rf "
+						+ " INNER JOIN d_rf_orgao_superior ON fato_receita_federal.id_orgao_superior_d_rf = d_rf_orgao_superior.id_orgao_superior_d_rf "
+//						+ " INNER JOIN d_rf_orgao_subordinado ON fato_receita_federal.id_orgao_subordinado_d_rf = d_rf_orgao_subordinado.id_orgao_subordinado_d_rf "
+						+ " INNER JOIN d_rf_unidade_gestora ON fato_receita_federal.id_unidade_gestora_d_rf = d_rf_unidade_gestora.id_unidade_gestora_d_rf "
+						+ " INNER JOIN d_rf_data ON fato_receita_federal.id_data_d_rf = d_rf_data.id_data_d_rf "
+						+ " INNER JOIN d_rf_mes ON fato_receita_federal.id_mes_d_rf = d_rf_mes.id_mes_d_rf "
+						+ " INNER JOIN d_rf_ano ON fato_receita_federal.id_ano_d_rf = d_rf_ano.id_ano_d_rf "
+//						+ " INNER JOIN d_rf_categoria_economica ON fato_receita_federal.id_categoria_economica_d_rf = d_rf_categoria_economica.id_categoria_economica_d_rf"
+						+ " LIMIT 20 OFFSET 0");
 
 		// Executa o SELECT
 		ResultSet rs = stmt.executeQuery();
@@ -82,10 +82,10 @@ public class Conversor {
 			receitaFed.setAlinea(rs.getString("cd_alinea"));
 			receitaFed.setSubalinea(rs.getString("cd_alinea"));
 			receitaFed.setOrigem(rs.getString("cd_alinea"));
-			receitaFed.setCategoriaEconomica(rs.getString("cd_alinea"));
+//			receitaFed.setCategoriaEconomica(rs.getString("cd_alinea"));
 			receitaFed.setOrgaoSuperior(rs.getString("cd_alinea"));
 			receitaFed.setAno(rs.getInt("cd_alinea"));
-			receitaFed.setOrgaoSubordinado(rs.getString("cd_alinea"));
+//			receitaFed.setOrgaoSubordinado(rs.getString("cd_alinea"));
 			receitaFed.setMes(rs.getString("cd_alinea"));
 			receitaFed.setUnidadeGestora(rs.getString("cd_alinea"));
 			receitaFed.setData(rs.getString("cd_alinea"));
@@ -105,46 +105,43 @@ public class Conversor {
 		// Funcao que realiza a query no banco de dados para obter todas as informacoes referentes a Despesas Federais
 
 		PreparedStatement stmt = con.prepareStatement(
-				"SELECT * FROM fato_despesa_federal "
-						+ "JOIN d_df_repasse "
-						+ "JOIN d_df_programa "
-						+ "JOIN d_df_portador "
-						+ "JOIN d_df_orgao_superior "
-						+ "JOIN d_df_orgao "
-						+ "JOIN d_df_orgao "
-						+ "JOIN d_df_nro_empenho "
-						+ "JOIN d_df_nro_convenio "
-						+ "JOIN d_df_municipio "
-						+ "JOIN d_df_modalidade_despesa "
-						+ "JOIN d_df_mes "
-						+ "JOIN d_df_linguagem_cidada"
-						+ "JOIN d_df_item_despesa "
-						+ "JOIN d_df_grupo_despesa "
-						+ "JOIN d_df_funcao "
-						+ "JOIN d_df_fonte_recurso "
-						+ "JOIN d_df_favorecidao_bolsa_familia "
-						+ "JOIN d_df_executor "
-						+ "JOIN d_df_elemento_despesa "
-						+ "JOIN d_df_data_pagameto "
-						+ "JOIN d_df_credor "
-						+ "JOIN d_df_convenente"
-						+ "JOIN d_df_categoria_despesa "
-						+ "JOIN d_df_ano "
-						+ "JOIN d_df_acao "
-						+ "JOIN d_df_unidade_gestora "
-						+ "JOIN d_df_uf "
-						+ "JOIN d_df_transacao "
-						+ "JOIN d_df_tipo_despesa "
-						+ "JOIN d_df_subtipo_despesa "
-						+ "JOIN d_df_subfuncao "
-						+ "JOIN d_df_situacao_parcela "
-						+ "LIMIT 20");
+				"SELECT * FROM fato_despesa_federal"
+						+ " JOIN d_df_repasse ON fato_despesa_federal.id_repasse_d_df = d_df_repasse.id_repasse_d_df"
+						+ " JOIN d_df_programa ON fato_despesa_federal.id_programa_d_df = d_df_programa.id_programa_d_df"
+						+ " JOIN d_df_portador ON fato_despesa_federal.id_portador_d_df = d_df_portador.id_portador_d_df"
+						+ " JOIN d_df_orgao_superior ON fato_despesa_federal.id_orgao_superior_d_df = d_df_orgao_superior.id_orgao_superior_d_df"
+						+ " JOIN d_df_orgao ON fato_despesa_federal.id_orgao_d_df = d_df_orgao.id_orgao_d_df"
+//						+ " JOIN d_df_orgao ON fato_despesa_federal.id_orgao_d_df = d_df_orgao.id_orgao_d_df"
+						+ " JOIN d_df_nro_empenho ON fato_despesa_federal.id_nro_empenho_d_df = d_df_nro_empenho.id_nro_empenho_d_df"
+						+ " JOIN d_df_nro_convenio ON fato_despesa_federal.id_nro_convenio_d_df = d_df_nro_convenio.id_nro_convenio_d_df"
+						+ " JOIN d_df_municipio ON fato_despesa_federal.id_municipio_d_df = d_df_municipio.id_municipio_d_df"
+						+ " JOIN d_df_modalidade_despesa ON fato_despesa_federal.id_modalidade_despesa_d_df = d_df_modalidade_despesa.id_modalidade_despesa_d_df"
+						+ " JOIN d_df_mes ON fato_despesa_federal.id_mes_d_df = d_df_mes.id_mes_d_df"
+						+ " JOIN d_df_linguagem_cidada ON fato_despesa_federal.id_linguagem_cidada_d_df = d_df_linguagem_cidada.id_linguagem_cidada_d_df"
+//						+ " JOIN d_df_item_despesa ON fato_despesa_federal.id_item_despesa_d_df = d_df_item_despesa.id_item_despesa_d_df"
+						+ " JOIN d_df_grupo_despesa ON fato_despesa_federal.id_grupo_despesa_d_df = d_df_grupo_despesa.id_grupo_despesa_d_df"
+						+ " JOIN d_df_funcao ON fato_despesa_federal.id_funcao_d_df = d_df_funcao.id_funcao_d_df"
+//						+ " JOIN d_df_fonte_recurso ON fato_despesa_federal.id_fonte_recurso_d_df = d_df_fonte_recurso.id_fonte_recurso_d_df"
+//						+ " JOIN d_df_favorecidao_bolsa_familia ON fato_despesa_federal.id_favorecido_bolsa_familia_d_df = d_df_favorecidao_bolsa_familia.id_favorecido_bolsa_familia_d_df"
+						+ " JOIN d_df_executor ON fato_despesa_federal.id_executor_d_df = d_df_executor.id_executor_d_df"
+						+ " JOIN d_df_elemento_despesa ON fato_despesa_federal.id_elemento_despesa_d_df = d_df_elemento_despesa.id_elemento_despesa_d_df"
+//						+ " JOIN d_df_data_pagameto ON fato_despesa_federal.id_data_pagamento_d_df = d_df_data_pagameto.id_data_pagamento_d_df"
+						+ " JOIN d_df_credor ON fato_despesa_federal.id_credor_d_df = d_df_credor.id_credor_d_df"
+						+ " JOIN d_df_convenente ON fato_despesa_federal.id_convenente_d_df = d_df_convenente.id_convenente_d_df"
+						+ " JOIN d_df_categoria_despesa ON fato_despesa_federal.id_categoria_despesa_d_df = d_df_categoria_despesa.id_categoria_despesa_d_df"
+						+ " JOIN d_df_ano ON fato_despesa_federal.id_ano_d_df = d_df_ano.id_ano_d_df"
+						+ " JOIN d_df_acao ON fato_despesa_federal.id_acao_d_df = d_df_acao.id_acao_d_df"
+						+ " JOIN d_df_unidade_gestora ON fato_despesa_federal.id_unidade_gestora_d_df = d_df_unidade_gestora.id_unidade_gestora_d_df"
+						+ " JOIN d_df_uf ON fato_despesa_federal.id_uf_d_df = d_df_uf.id_uf_d_df"
+						+ " JOIN d_df_transacao ON fato_despesa_federal.id_transacao_d_df = d_df_transacao.id_transacao_d_df"
+						+ " JOIN d_df_tipo_despesa ON fato_despesa_federal.id_tipo_despesa_d_df = d_df_tipo_despesa.id_tipo_despesa_d_df"
+						+ " JOIN d_df_subtipo_despesa ON fato_despesa_federal.id_subtipo_despesa_d_df = d_df_subtipo_despesa.id_subtipo_despesa_d_df"
+						+ " JOIN d_df_subfuncao ON fato_despesa_federal.id_subfuncao_d_df = d_df_subfuncao.id_subfuncao_d_df"
+						+ " JOIN d_df_situacao_parcela ON fato_despesa_federal.id_situacao_parcela_d_df = d_df_situacao_parcela.id_situacao_parcela_d_df"
+						+ " LIMIT 20 OFFSET 0");
 
 		// Executa o select
 		ResultSet rs = stmt.executeQuery();
-
-		rs.close();
-		stmt.close();
 
 		ArrayList<DespesaFed> despesasFed = new ArrayList<>();
 
@@ -187,6 +184,9 @@ public class Conversor {
 
 			despesasFed.add(despesaFed);
 		}
+		
+		rs.close();
+		stmt.close();
 
 		return despesasFed;
 	}
@@ -212,7 +212,7 @@ public class Conversor {
 			recurso.addProperty(temAlinea, receitaFed.getAlinea());
 			recurso.addProperty(temOrigem, receitaFed.getOrigem());
 			recurso.addProperty(temSubalinea, receitaFed.getSubalinea());
-			recurso.addProperty(temCategoriaEconomicaDaReceita, receitaFed.getCategoriaEconomica());
+//			recurso.addProperty(temCategoriaEconomicaDaReceita, receitaFed.getCategoriaEconomica());
 			recurso.addProperty(temRubrica, receitaFed.getRubrica());
 			recurso.addProperty(temEspecie, receitaFed.getEspecie());
 			recurso.addLiteral(ano, receitaFed.getAno());
